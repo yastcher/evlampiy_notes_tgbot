43ba1a5011ebbfc5d8a1806c63077eeb9d8558b1
Yuri Dremov <yastcher@gmail.com>
2024-04-30 06:50:36 +0300

    init

---
+++ src/__main__.py
@@ -0,0 +1,38 @@
+import logging
+
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+from src import handlers, config
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+COMMAND_HANDLERS = {
+    "start": handlers.start,
+    "help": handlers.help_info,
+    # "evlampiy": handlers.command_evlampiy,
+}
+
+
+if not config.TELEGRAM_BOT_TOKEN:
+    raise ValueError("need TELEGRAM_BOT_TOKEN env variables")
+
+
+def main():
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+
+    for command_name, command_handler in COMMAND_HANDLERS.items():
+        application.add_handler(CommandHandler(command_name, command_handler))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        import traceback
+
+        logger.warning(traceback.format_exc())
---
+++ src/handlers.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from telegram import InlineKeyboardMarkup, Update
+from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
+
+
+async def send_response(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    response: str,
+    keyboard: Optional[InlineKeyboardMarkup] = None
+) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=response,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+        reply_markup=keyboard
+    )
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_response(update, context, response="I translate a voice message to text")
+
+
+async def help_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_response(update, context, response="/start - Start the bot\n/help - Show this help message")
